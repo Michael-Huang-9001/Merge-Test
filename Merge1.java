ccdfa2d330ac488a44e42e79a19c10eb1dab8e22
Michael Huang <mihuang@c02nkdakg3qd.corp.proofpoint.com>
2018-04-12 08:07:45 -0700

    added test2

--- Merge1.java
+++ Merge1.java
@@ -1,8 +1,8 @@
 public class Merge1 {
 
     public static void main(String[] args) {
-        System.out.println(test1(1,2));
-        // System.out.println(test2(1,2));
+        System.out.println(test1(1, 2));
+        System.out.println(test2(1, 2));
     }
 
     public static int test1(int a, int b) {
@@ -10,5 +10,12 @@ public class Merge1 {
         sum += 1;
         sum -= 1;
         return sum;
+    }
+
+    public static int test2(int a, int b) {
+        int product = a * b;
+        product += 1;
+        product -= 1;
+        return product;
     }
 }
