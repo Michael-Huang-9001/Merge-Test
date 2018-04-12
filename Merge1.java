5e9b74b554622abd9b09f54f03368b143c38e0cf
Michael Huang <mihuang@c02nkdakg3qd.corp.proofpoint.com>
2018-04-12 07:48:15 -0700

    commit1, testing for merge conflicts

---
+++ Merge1.java
@@ -0,0 +1,11 @@
+public class Merge1 {
+
+    public static void main(String[] args) {
+        System.out.println(test1(1,2));
+    }
+
+    public static int test1(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+}
