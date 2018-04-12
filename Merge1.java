f3c7fdcc02d78c23faf12c63ed523ad454638844
Michael Huang <mihuang@c02nkdakg3qd.corp.proofpoint.com>
2018-04-12 07:54:42 -0700

    change on master

--- Merge1.java
+++ Merge1.java
@@ -2,10 +2,16 @@ public class Merge1 {
 
     public static void main(String[] args) {
         System.out.println(test1(1,2));
+        System.out.println(test2(1,2));
     }
 
     public static int test1(int a, int b) {
         int sum = a + b;
         return sum;
+    }
+
+    public static int test2(int c, int d) {
+        int product = c * d;
+        return product;
     }
 }
