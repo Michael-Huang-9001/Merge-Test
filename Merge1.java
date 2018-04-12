8b08d11da407dd661c2a39b1e32706c66ed44a48
Michael Huang <mihuang@c02nkdakg3qd.corp.proofpoint.com>
2018-04-12 08:02:29 -0700

    to master

--- Merge1.java
+++ Merge1.java
@@ -2,7 +2,7 @@ public class Merge1 {
 
     public static void main(String[] args) {
         System.out.println(test1(1,2));
-        System.out.println(test2(1,2));
+        // System.out.println(test2(1,2));
     }
 
     public static int test1(int a, int b) {
